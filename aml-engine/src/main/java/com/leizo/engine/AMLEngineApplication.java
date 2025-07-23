e6dd927295b341c857aafa9ceefd5c15a97f485f
Leizor-domain <x23287951@student.ncirl.ie>
2025-07-23 21:12:59 +0100

    fix: add Spring Boot main class and executable JAR build for aml-engine

---
+++ aml-engine/src/main/java/com/leizo/engine/AMLEngineApplication.java
@@ -0,0 +1,11 @@
+package com.leizo.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AMLEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AMLEngineApplication.class, args);
+    }
+} 

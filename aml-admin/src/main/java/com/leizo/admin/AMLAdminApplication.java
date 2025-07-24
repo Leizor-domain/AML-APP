165ed91c566af878297f503b0360a167d75a754b
Leizor-domain <x23287951@student.ncirl.ie>
2025-07-24 05:41:14 +0100

    fix: enable component, repository, and entity scanning for shared beans in aml-admin

--- aml-admin/src/main/java/com/leizo/admin/AMLAdminApplication.java
+++ aml-admin/src/main/java/com/leizo/admin/AMLAdminApplication.java
@@ -2,11 +2,18 @@ package com.leizo.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //Spring boot Admin Application
-@SpringBootApplication(scanBasePackages = {"com.leizo"})
-@EnableJpaRepositories(basePackages = {"com.leizo.admin.repository"})
+@SpringBootApplication(scanBasePackages = {
+    "com.leizo.admin",
+    "com.leizo.common.security",
+    "com.leizo.common.repository",
+    "com.leizo.common.entity"
+})
+@EnableJpaRepositories(basePackages = "com.leizo.common.repository")
+@EntityScan(basePackages = "com.leizo.common.entity")
 public class AMLAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AMLAdminApplication.class, args);

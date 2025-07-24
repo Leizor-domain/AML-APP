f2d8f023163917d8b9258ab9f507bfe3137966a6
Leizor-domain <x23287951@student.ncirl.ie>
2025-07-24 05:10:23 +0100

    fix: enable JPA repository and entity scanning for shared user auth in aml-portal

--- aml-portal/src/main/java/com/leizo/portal/AMLPortalApplication.java
+++ aml-portal/src/main/java/com/leizo/portal/AMLPortalApplication.java
@@ -1,14 +1,13 @@
 package com.leizo.portal;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//Spring boot Portal Application
-@SpringBootApplication(scanBasePackages = {
-        "com.leizo.portal",
-        "com.leizo.common.security" // <- INCLUDE THIS
-})
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.leizo.common.repository")
+@EntityScan(basePackages = "com.leizo.common.entity")
 public class AMLPortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(AMLPortalApplication.class, args);

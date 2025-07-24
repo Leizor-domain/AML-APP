c7faf2172853e2eb9e17d306243fb7e86f5476a9
Leizor-domain <x23287951@student.ncirl.ie>
2025-07-24 05:26:21 +0100

    fix: enable component scanning for JwtAuthFilter and shared beans in aml-portal

--- aml-portal/src/main/java/com/leizo/portal/AMLPortalApplication.java
+++ aml-portal/src/main/java/com/leizo/portal/AMLPortalApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "com.leizo.portal",
+    "com.leizo.common.security",
+    "com.leizo.common.repository",
+    "com.leizo.common.entity"
+})
 @EnableJpaRepositories(basePackages = "com.leizo.common.repository")
 @EntityScan(basePackages = "com.leizo.common.entity")
 public class AMLPortalApplication {

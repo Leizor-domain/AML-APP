4b84b6c694f0ba1b8d27232303cf1d481f06cf3d
Leizor-domain <x23287951@student.ncirl.ie>
2025-07-25 12:09:53 +0100

    fix: add @ComponentScan(basePackages = {'com.leizo'}) to AMLAdminApplication to ensure all beans are discovered

--- aml-admin/src/main/java/com/leizo/admin/AMLAdminApplication.java
+++ aml-admin/src/main/java/com/leizo/admin/AMLAdminApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
 //Spring boot Admin Application
 @SpringBootApplication(scanBasePackages = {
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
     "com.leizo.service",
     "com.leizo.admin.repository"
 })
+@ComponentScan(basePackages = {"com.leizo"})
 @EnableJpaRepositories(basePackages = {
     "com.leizo.common.repository",
     "com.leizo.admin.repository"

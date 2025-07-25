d5db170c67dae4bbd13d45484e8136b5625380e2
Leizor-domain <x23287951@student.ncirl.ie>
2025-07-25 03:28:56 +0100

    fix(aml-portal): remove JPA imports and annotations from AMLPortalApplication, as JPA is not needed in portal module

--- aml-portal/src/main/java/com/leizo/portal/AMLPortalApplication.java
+++ aml-portal/src/main/java/com/leizo/portal/AMLPortalApplication.java
@@ -2,17 +2,10 @@ package com.leizo.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
-    "com.leizo.portal",
-    "com.leizo.common.security",
-    "com.leizo.common.repository",
-    "com.leizo.common.entity"
+    "com.leizo.portal"
 })
-@EnableJpaRepositories(basePackages = "com.leizo.common.repository")
-@EntityScan(basePackages = "com.leizo.common.entity")
 public class AMLPortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(AMLPortalApplication.class, args);
